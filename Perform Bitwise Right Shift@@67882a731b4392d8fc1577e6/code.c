c4d326346a37bc53e7ab743f152e076aad1b8dd2
deepak c. <deepakchoudhary3365@gmail.com>
2025-02-18 00:28:21 +0530

    Solution to Perform Bitwise Right Shift

---
+++ Perform Bitwise Right Shift@@67882a731b4392d8fc1577e6/code.c
@@ -0,0 +1,14 @@
+// Your code here...
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    // Input two integers
+    scanf("%d %d", &num1, &num2);
+
+    // Print the result of num1 XOR num2
+    printf("%d", num1 >>num2);
+
+    return 0;
+}
